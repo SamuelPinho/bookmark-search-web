0347cd91f85e0cc706f78fbafc11b72b5057743a
Samuel Monteiro <samuel.monteiro@mediavalet.com.br>
2022-05-17 23:05:18 -0300

    feature: transform into two columns

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,26 +25,9 @@ export const Layout = ({ children }: LayoutProps) => {
     <ChakraProvider theme={theme}>
       <CSSReset />
       <Flex bgColor={"gray.800"} flexDir="column" minH="100vh">
-        {/* <nav>
-          <Flex
-            pos={"sticky"}
-            top="0"
-            w="100%"
-            bgColor={"gray.700"}
-            px={4}
-            py={1}
-            zIndex={"docked"}
-            shadow={"base"}
-            alignItems="center"
-          >
-            <Text as="h2" fontWeight={"bold"} fontSize="xl">
-              Bookmark search
-            </Text>
-          </Flex>
-        </nav> */}
         <main>
           <Flex flex="1">
-            <Flex py="10" px="6" w="full" h="100vh">
+            <Flex w="full" h="100vh">
               {children}
             </Flex>
           </Flex>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,35 +13,44 @@ const Home: NextPage = () => {
         w="full"
         alignItems={"center"}
         justifyContent={"space-between"}
-        flexDir="column"
+        flexDir="row"
       >
-        <Text fontWeight={"bold"} fontSize="4xl">
-          Superpower your browser
-        </Text>
-        <Text
-          maxW={"500px"}
-          textAlign="center"
-          fontSize={"xl"}
-          textColor="gray.400"
-        >
-          A browser extension that allows fast searcing of bookmarks, history
-          and much more
-        </Text>
-        <Image
-          src="/demo.gif"
-          alt="demo app"
-          width={629}
-          height={461}
-          borderRadius="lg"
-          boxShadow={"base"}
-          mt="2"
-        />
         <Flex
-          w="full"
+          flexDir={"column"}
+          w="50%"
+          alignItems={"center"}
+          height="full"
+          justifyContent={"center"}
+        >
+          <Text fontWeight={"bold"} fontSize="4xl">
+            Superpower your browser
+          </Text>
+          <Text
+            maxW={"500px"}
+            textAlign="center"
+            fontSize={"xl"}
+            textColor="gray.400"
+          >
+            A browser extension that allows fast searcing of bookmarks, history
+            and much more
+          </Text>
+          <Image
+            src="/demo.gif"
+            alt="demo app"
+            width={629}
+            height={461}
+            borderRadius="lg"
+            boxShadow={"base"}
+            mt="16"
+          />
+        </Flex>
+        <Flex
+          bg="gray.900"
+          h="full"
+          w="50%"
           flexDir={"row"}
           alignItems="center"
           justifyContent={"center"}
-          mt="4"
           columnGap={36}
         >
           <PerkBox
