db7a7ee17060ec803daa0781e8f193849c218378
Samuel Monteiro <samuel.monteiro@mediavalet.com.br>
2022-07-22 00:19:56 -0300

    fix: color mode

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import {
   ChakraProvider,
+  ColorModeProvider,
   ColorModeScript,
   CSSReset,
+  DarkMode,
   extendTheme,
   Flex,
+  Text,
   ThemeConfig,
 } from "@chakra-ui/react";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -26,15 +30,17 @@ export const Layout = ({ children }: LayoutProps) => {
       <ColorModeScript initialColorMode={theme.initialColorMode} />
       <ChakraProvider theme={theme}>
         <CSSReset />
-        <Flex flexDir="column" minH="100vh" bg="#151d29">
-          <main>
-            <Flex flex="1">
-              <Flex w="full" h="100vh">
-                {children}
+        <DarkMode>
+          <Flex flexDir="column" minH="100vh" bg="#151d29">
+            <main>
+              <Flex flex="1">
+                <Flex w="full" h="100vh">
+                  {children}
+                </Flex>
               </Flex>
-            </Flex>
-          </main>
-        </Flex>
+            </main>
+          </Flex>
+        </DarkMode>
       </ChakraProvider>
     </>
   );
